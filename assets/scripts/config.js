ab30eeedcc1865ee67283012bc75b6ba94ee0333
lizcramerfox <ecfox2008@gmail.com>
2020-04-02 12:31:20 -0400

    Added Project Planning
    Folder: 0-Project Admin
    - Requirements.md
    - Schedule.md

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: '<replace-with-heroku-url>',
+  production: 'https://apricot-custard-90647.herokuapp.com/',
   development: 'http://localhost:4741'
 }
 
